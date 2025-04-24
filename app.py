a1b5e5f0f601e7b763e0b913e4ec2871bcb96005
olni0002 <olni0002@stud.kea.dk>
2025-04-24 12:40:30 +0200

    New endpoints.

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, request
 import numpy as np
 
-start_board = np.zeros((8,8))
+import game
+
+start_board = np.zeros((8,8), dtype="uint8")
 start_board[[0,-1],1:-1] = 1
 start_board[1:-1,[0,-1]] = 2
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def getBoard():
+@app.route('/api/board/')
+def get_board():
     return {'board': start_board.tolist()}
+
+@app.route('/api/validateMove', methods=['POST'])
+def validate_move():
+    board = request.get_json()
+
+    is_valid: bool = game.is_move_legal(board["start_row"], board["start_col"], board["dest_row"],
+                                        board["dest_col"], board["board"])
+    return {"is_valid": is_valid}
+
+@app.route('/api/computerMove', methods=['POST'])
+def computer_move():
+    board = request.get_json()["board"]
+
+    board = game.computer_move(np.array(board, dtype="U1"))
+
+    board = list(map(lambda row: list(map(lambda field: None if field == "N" else field, row)), board.tolist()))
+    return {'board': board}
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ game.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+def legal_moves(start_row, start_col, board: np.ndarray):
+    fields = []
+    board_height = board.shape[0]
+    board_width = board.shape[1]
+
+    row = board[start_row]
+    col = board[:,start_col]
+
+    diag_lower_left = board[start_row:,start_col::-1].diagonal()
+    diag_upper_right = board[start_row::-1,start_col:].diagonal()
+    diag1 = np.concatenate((diag_lower_left[1:], diag_upper_right))
+
+    diag_lower_right = board[start_row:,start_col:].diagonal()
+    diag_upper_left = board[start_row::-1,start_col::-1].diagonal()
+    diag2 = np.concatenate((diag_lower_right[1:], diag_upper_left))
+
+
+    pieces_in_row = 0
+    for i in row:
+        if i != 'N':
+            pieces_in_row += 1
+
+    new_col = start_col + pieces_in_row
+    if new_col >= 0 and new_col < board_width:
+        fields.append((start_row, new_col))
+
+    new_col = start_col - pieces_in_row
+    if new_col >= 0 and new_col < board_width:
+        fields.append((start_row, new_col))
+
+    pieces_in_col = 0
+    for i in col:
+        if i != 'N':
+            pieces_in_col += 1
+
+    new_row = start_row + pieces_in_col
+    if new_row >= 0 and new_row < board_height:
+        fields.append((new_row, start_col))
+
+    new_row = start_row - pieces_in_col
+    if new_row >= 0 and new_row < board_height:
+        fields.append((new_row, start_col))
+
+    pieces_in_diag1 = 0
+    for i in diag1:
+        if i != 'N':
+            pieces_in_diag1 += 1
+
+    new_row = start_row - pieces_in_diag1
+    new_col = start_col + pieces_in_diag1
+    if new_row >= 0 and new_row < board_height and new_col >= 0 and new_col < board_width:
+        fields.append((new_row, new_col))
+
+    new_row = start_row + pieces_in_diag1
+    new_col = start_col - pieces_in_diag1
+    if new_row >= 0 and new_row < board_height and new_col >= 0 and new_col < board_width:
+        fields.append((new_row, new_col))
+
+    pieces_in_diag2 = 0
+    for i in diag2:
+        if i != 'N':
+            pieces_in_diag2 += 1
+
+    new_row = start_row - pieces_in_diag2
+    new_col = start_col - pieces_in_diag2
+    if new_row >= 0 and new_row < board_height and new_col >= 0 and new_col < board_width:
+        fields.append((new_row, new_col))
+
+    new_row = start_row + pieces_in_diag2
+    new_col = start_col + pieces_in_diag2
+    if new_row >= 0 and new_row < board_height and new_col >= 0 and new_col < board_width:
+        fields.append((new_row, new_col))
+
+    new_fields = []
+    for field_row, field_col in fields:
+        if board[start_row, start_col] != board[field_row, field_col]:
+            new_fields.append((field_row, field_col))
+
+
+
+    return new_fields
+
+
+
+
+def is_move_legal(start_row, start_col, dest_row, dest_col, board) -> bool:
+    board = np.array(board, dtype="U1")
+
+
+
+    if (dest_row, dest_col) in legal_moves(start_row, start_col, board):
+        return True
+    else:
+        return False
+
+def computer_move(board) -> np.ndarray:
+
+    board = np.array(board, dtype="U1")
+    piece_coordinates = []
+
+    for row_index, row in enumerate(board):
+        for col_index, field in enumerate(row):
+            if board[row_index, col_index] == 'W':
+                piece_coordinates.append((row_index, col_index))
+
+    moves = []
+    for row_index, col_index in piece_coordinates:
+        dest = legal_moves(row_index, col_index, board)
+
+        moves.append((row_index, col_index, dest))
+
+    best_move = moves[0]
+
+    board[best_move[0],best_move[1]] = "N"
+    board[best_move[2][0],best_move[2][1]] = "W"
+
+    return board
